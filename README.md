181e26aa1978dc44e804359ba4a9420062d03a28
uyghur-io <67357374+uyghur-io@users.noreply.github.com>
2020-12-20 20:27:44 +0300

    -

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # React Hooks
-It can affect states outside the scope, etc.
+It can affect states outside the scope, etc.  
+install `npm install react-highlight.js --save`
+usage  
+```js
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+const Component = () => {
+  const codeString = '(num) => num + 1';
+  return (
+    <SyntaxHighlighter language="javascript" style={docco}>
+      {codeString}
+    </SyntaxHighlighter>
+  );
+};
+```
 ## useEffect
--- src/Hookis/useEffect/useEffect.js
+++ src/Hookis/useEffect/useEffect.js
@@ -1,10 +1,41 @@
 import React, { useEffect, useState } from 'react'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { tomorrowNight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+const code1 = `import React, { useEffect, useState } from 'react'
+
+function Example() {
+    const [count, setCount] = useState(0)
+  
+    useEffect(() => {
+     document.title = \`You Clicked  \${count} times\`
+    })
+    
+    return (
+        <div>
+            <p>Click button to change html API document.title .</p>
+            <button onClick = { () => setCount(count + 1) } >Click</button>
+        </div>
+    )
+    `
+
+    const code2 = `const [dir, setDir] = useState("ltr")
+
+    useEffect(() => {
+        document.dir = \`\${dir}\`
+       // console.log(a)
+       })`
 
 
 export default function useEffectExample() {
     return (
         <div>
+           <h3>
+               useEffect
+           </h3>
             <Example />
+            <hr />
+            <Example2 />
         </div>
     )
 }
@@ -22,17 +53,54 @@ function Example() {
    useEffect(() => {
      document.dir = `${dir}`
     // console.log(a)
-})
+    })
+
+    useEffect(() => {
+    console.log('just excuit frist time')
+    }, [])
    
    return (
        <div>
-           <h3>
-               useEffect
-           </h3>
+           <SyntaxHighlighter language="javascript" style={tomorrowNight}>
+            {code1}
+           </SyntaxHighlighter>
            <p>Click button to change html API document.title .</p>
            <button onClick={ () => setCount(count + 1) } >Click</button>
+           <SyntaxHighlighter language="javascript" style={tomorrowNight}>
+            {code2}
+           </SyntaxHighlighter>
            <p>Click button to change html API document.dir .</p>
            <button onClick={ () => setDir("rtl") }>Click</button>
+           <p>Only excuit on first time !</p>
+           <SyntaxHighlighter language="javascript" style={tomorrowNight}>
+            {`useEffect(() => {
+    console.log('just excuit frist time')
+}, [])`}
+            </SyntaxHighlighter>
        </div>
    )
+}
+
+class Example2 extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count : 0
+        }
+    }
+
+    // excuit first time
+    componentDidMount() {
+        document.title = `You clicked ${this.state.count} (time, fist time)`
+    }
+
+    // excuit ever time
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 }
