5ccbe551dc598d2aad10b4f51ace46c0d5c487c7
uyghur-io <admin@uyghur.io>
2020-12-21 20:22:03 +0300

    -

---
+++ src/Hookis/useEffect/examples/Subscribe2.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import ChatAPI from './ChatAPI';
+
+// Old way
+class FriendStatus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOnline: null };
+  }
+
+  componentDidMount() {
+    ChatAPI.subscribeToFriendStatus(
+      this.props.friend.id,
+      this.handleStatusChange
+    );
+  }
+
+  componentWillUnmount() {
+    ChatAPI.unsubscribeFromFriendStatus(
+      this.props.friend.id,
+      this.handleStatusChange
+    );
+  }
+
+  handleStatusChange = status => {
+    this.setState({
+      isOnline: status.isOnline,
+    });
+  };
+
+  render() {
+    if (this.state.isOnline === null) {
+      return 'Loading...';
+    }
+    return this.state.isOnline ? 'Online' : 'Offline';
+  }
+}
+
+// New way
+function FriendStatusHooks(props) {
+  const [isOnline, setIsOnline] = useState(null);
+  useEffect(() => {
+    const handleStatusChange = status => setIsOnline(status.isOnline);
+    ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+
+    // Specify how to clean up after this effect:
+    return () =>
+      ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+  }, [props.friend.id]);
+
+  if (isOnline === null) {
+    return 'Loading...';
+  }
+  return isOnline ? 'Online' : 'Offline';
+}
+
+// New way with custom hook
+function useFriendStatus(friendId) {
+  const [isOnline, setIsOnline] = useState(null);
+  useEffect(() => {
+    const handleStatusChange = status => setIsOnline(status.isOnline);
+    ChatAPI.subscribeToFriendStatus(friendId, handleStatusChange);
+
+    // Specify how to clean up after this effect:
+    return () =>
+      ChatAPI.unsubscribeFromFriendStatus(friendId, handleStatusChange);
+  }, [friendId]);
+  return isOnline;
+}
+
+function FriendStatusCustomHook(props) {
+  const isOnline = useFriendStatus(props.friend.id);
+
+  if (isOnline === null) {
+    return 'Loading...';
+  }
+  return isOnline ? 'Online' : 'Offline';
+}
+
+export default function Subscribe2(props) {
+  const changingId = useChangingId();
+  return (
+    <>
+      <div>
+        <FriendStatus friend={{ id: 42 }} />
+      </div>
+      <div>
+        <FriendStatus friend={{ id: 43 }} />
+      </div>
+      <div>
+        <FriendStatusHooks friend={{ id: 123 }} />
+      </div>
+      <div>
+        <FriendStatusCustomHook friend={{ id: changingId }} />
+      </div>
+    </>
+  );
+}
+
+function useChangingId() {
+  const [id, setId] = useState(1);
+  useEffect(() => {
+    setTimeout(() => setId(id => id + 1), 5700);
+  });
+  return id;
+}
--- src/Hookis/useEffect/useEffect.js
+++ src/Hookis/useEffect/useEffect.js
@@ -4,6 +4,7 @@ import Example from "../useEffect/examples/Example";
 import ExampleOld from "../useEffect/examples/ExampleOld";
 import GetGithubUser from '../useEffect/examples/GetGithubUser'
 import Subscribe from '../useEffect/examples/Subscribe'
+import Subscribe2 from '../useEffect/examples/Subscribe2'
 
 export default function useEffectExample() {
     return (
@@ -15,6 +16,7 @@ export default function useEffectExample() {
                     <Route path="/useEffect/ExampleOld" component={ExampleOld}/>
                     <Route path="/useEffect/GetGithubUser" component={GetGithubUser}/>
                     <Route path="/useEffect/Subscribe" component={Subscribe}/>
+                    <Route path="/useEffect/Subscribe2" component={Subscribe2}/>
                 </Switch>
             </Router>
         </>
@@ -30,5 +32,6 @@ const Home = () => (
       <Link to="/useEffect/ExampleOld"><li>Example Old</li></Link>
       <Link to="/useEffect/GetGithubUser"><li>Get Github Users</li></Link>
       <Link to="/useEffect/Subscribe"><li>Subscribe</li></Link>
+      <Link to="/useEffect/Subscribe2"><li>Subscribe2</li></Link>
     </div>
   )
